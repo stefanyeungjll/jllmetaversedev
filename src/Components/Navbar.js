e2958066277dcec6ee9828c95c499fe1b50d2698
AP\Stefan.Yeung <stefan.yeung@ap.jll.com>
2022-05-16 15:11:35 +0800

    add nav bar and fix phone camera problem.

---
+++ src/Components/Navbar.js
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import styled from "styled-components";
+
+const Navbartext = styled.div`
+    margin-left: 10px;
+`
+export default function ButtonAppBar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+            <Navbartext>
+                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                JLL Metaverse
+                </Typography>
+            </Navbartext>
+          
+          {/* <Button color="inherit">Login</Button> */}
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
