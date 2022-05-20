d7f5dd9448fa6e9a106832e8a656e2a242beb53c
AP\Stefan.Yeung <stefan.yeung@ap.jll.com>
2022-05-20 09:46:40 +0800

    Add mappage and remove extra text

--- src/Page/HomePage.js
+++ src/Page/HomePage.js
@@ -1,8 +1,15 @@
 import styled from "styled-components";
-import Camera from "../Components/Display";
+// import Camera from "../Components/Display";
 import React, { useState } from "react";
 import SearchAppBar from "../Components/Navbar";
+import {
+  First,
+  Second,
+  Third,
+  Fourth
+} from '../assets/Images';
 
+import BackgroundSlider from 'react-background-slider'
 const Welcometext = styled.button`
   position: relative;
   margin-top: 100px;
@@ -15,16 +22,27 @@ const Welcometext = styled.button`
   border-radius: 8px;
 `;
 
+const LinkWrapper = styled.a`
+  color: #FFFFFF;
+  text-decoration: none;
+`;
+
 export default function HomeLanding(){
     const [iscameraOpen, setIscameraOpen] = useState(false);
     console.log(iscameraOpen);
     return (
       <div>
+        <BackgroundSlider 
+        images={[First, Second, Third, Fourth]}
+        duration={10}
+        transition={2}
+        >
+        </BackgroundSlider>
         <SearchAppBar></SearchAppBar>
         <Welcometext onClick={() => setIscameraOpen((prev) => !prev)}>
-          2030+ Conceptual Development
+          <LinkWrapper href="3dbuildings.html">2030+ Conceptual Development</LinkWrapper>
         </Welcometext>
-        {iscameraOpen && <Camera></Camera>}
+        {/* {iscameraOpen && <Camera></Camera>} */}
       </div>
     );
   }
