c00fbf18b0ae80f4e0a95dff78a4fd8e4c5282a8
AP\Stefan.Yeung <stefan.yeung@ap.jll.com>
2022-06-01 15:39:41 +0800

    add icon and dashboard

--- src/Page/HomePage.js
+++ src/Page/HomePage.js
@@ -2,17 +2,32 @@ import styled from "styled-components";
 // import Camera from "../Components/Display";
 import React, { useState } from "react";
 // import SearchAppBar from "../Components/Navbar"; might be userful for later
-import Typography from '@material-ui/core/Typography';
-import { withStyles } from "@material-ui/core/styles";
 import Logo from "../assets/JLLlogo.png";
+import {
+  Wechaticon,
+  Linkedinicon
+} from '../assets/Images';
 
-const bodytext = "Bread flour is the best flour for homemade pizza dough. You can use all-purpose flour instead of the bread flour called for in the recipe, but bread flour is higher in gluten than all-purpose flour and will make a crispier crust for your pizza."
-const WhiteTextTypography = withStyles({
-  root: {
-    color: "#FFFFFF"
+const bodytext = "We incorporate big data with Geographic Information System (GIS) to provide accurate analysis and insights of different market sectors, enabling you to make the best-informed business decisions. GIS Consultancy includes but not limited to site selections, portfolio and competitor analysis."
+
+
+
+const WhiteTextTypography = styled.p`
+  position: relative;
+  margin-top: 20px;
+  color: #FFFFFF;
+  align-content: center;
+  text-align: center;
+  font-size: 10px;
+  padding-left: 10px;
+  padding-right: 10px;
+  @media screen 
+  and (min-device-width: 1264px) 
+  and (max-device-width: 1600px) 
+  and (-webkit-min-device-pixel-ratio: 1) { 
+    font-size: 52px;
   }
-})(Typography);
-
+`
 const HoverButton = styled.button`
   position: relative;
   margin-top: 20px;
@@ -21,15 +36,23 @@ const HoverButton = styled.button`
   align-content: center;
   text-align: center;
   height: 50px;
+  border: 2px solid #FFFFFF;
   border-radius: 15px;
-  
   @media screen 
   and (min-device-width: 375px) 
-  and (max-device-width: 414px) 
+  and (max-device-width: 768px) 
   and (-webkit-min-device-pixel-ratio: 1) { 
     font-size: 10px;
     width: 335px;
-}
+  }
+  &:hover {
+    background: Transparent no-repeat;
+    color: #FFFFFF;
+  }
+  &:focus {
+    background: Transparent no-repeat;
+    color: #FFFFFF;
+  }
 `;
 
 const Title = styled.p`
@@ -48,12 +71,12 @@ const Title = styled.p`
 
 `;
 const LinkWrapper = styled.a`
-  color: #FFFFFF;
   text-decoration: none;
 `;
 
 const PageWrapper = styled.div`
   position: relative;
+  width: 100%;
   max-width: 1264px;
   overflow: hidden;
   height: 100vh;
@@ -61,14 +84,65 @@ const PageWrapper = styled.div`
   background: rgba(0,0,0,.5);
 `;
 
-const JLLLogo = styled.img`
-  position: absolute;
-  left: 0;
-  top: 0;
-  width:136px;
-  height: 84px;
+const BodyWrapper = styled.div`
+  position: relative;
+  width: 90%;
+  max-width: 1264px;
+  overflow: hidden;
+  height: 100vh;
+  margin: auto;
 `;
 
+const JLLLogo = styled.img`
+  position: absolute;
+  object-fit: contain;
+  left: 0;
+  top: 0;
+  width: 85px;
+  height: 54px;
+`;
+
+const Wechaticonimage = styled.img`
+  position: relative;
+  object-fit: contain;
+  @media screen 
+  and (min-device-width: 375px) 
+  and (max-device-width: 768px) 
+  and (-webkit-min-device-pixel-ratio: 1) { 
+    width: 45px;
+    height: 44px;
+  }
+  width: 54px;
+  height: 34px;
+`;
+
+const Linkediniconimage = styled.img`
+  position: relative;
+  object-fit: contain;
+  @media screen 
+  and (min-device-width: 375px) 
+  and (max-device-width: 768px) 
+  and (-webkit-min-device-pixel-ratio: 1) { 
+    width: 31px;
+    height: 30px;
+    margin-left: 15px;
+    padding-bottom: 5px;
+  }
+  width: 54px;
+  height: 34px;
+`;
+
+const Icongroupwrapper = styled.div`
+  position: absolute;
+  margin-top: 5px;
+  margin-right: 4px;
+  right: 0;
+`;
+
+const ButtongroupWrapper = styled.div`
+  position: relative;
+  height: 500px;
+`;
 export default function HomeLanding(){
     const [iscameraOpen, setIscameraOpen] = useState(false);
     console.log(iscameraOpen);
@@ -76,20 +150,35 @@ export default function HomeLanding(){
       <PageWrapper>
         <JLLLogo src={Logo}></JLLLogo>
         {/* <SearchAppBar></SearchAppBar> */}
-        <Title>GIS Consultancy</Title>
-        <WhiteTextTypography variant="body2">
-          {bodytext}
-        </WhiteTextTypography>
-        <HoverButton onClick={() => setIscameraOpen((prev) => !prev)}>
-          <LinkWrapper href="3dbuildings.html">The Map</LinkWrapper>
-        </HoverButton>
-        <HoverButton onClick={console.log('hello clicked second button')}>
-          <LinkWrapper href="https://www.jll.com.hk/">JLL Official Website</LinkWrapper>
-        </HoverButton>
-        <HoverButton onClick={console.log('hello clicked second button')}>
-          <LinkWrapper href="https://www.jll.com.hk/">GIS Consultancy Service</LinkWrapper>
-        </HoverButton>
-        {/* {iscameraOpen && <Camera></Camera>} */}
+        <BodyWrapper>
+          <Title>GIS Consultancy</Title>
+            <WhiteTextTypography variant="body2">
+              {bodytext}
+            </WhiteTextTypography>
+            <ButtongroupWrapper>
+              <HoverButton onClick={() => setIscameraOpen((prev) => !prev)}>
+                <LinkWrapper href="3dbuildings.html">The Map</LinkWrapper>
+              </HoverButton>
+              <HoverButton onClick={console.log('hello clicked second button')}>
+                <LinkWrapper href="https://www.jll.com.hk/">JLL Official Website</LinkWrapper>
+              </HoverButton>
+              <HoverButton onClick={console.log('hello clicked second button')}>
+                <LinkWrapper href="https://www.jll.com.hk/">GIS Consultancy Service</LinkWrapper>
+              </HoverButton>
+              <HoverButton onClick={console.log('hello clicked second button')}>
+                <LinkWrapper href="https://www.jll.com.hk/">Dashboard</LinkWrapper>
+              </HoverButton>
+              {/* {iscameraOpen && <Camera></Camera>} */}
+              <Icongroupwrapper>
+                <a href="https://google.com">
+                  <Wechaticonimage src={Wechaticon}></Wechaticonimage>
+                </a>
+                <a href="https://www.linkedin.com/company/jll-hong-kong/">
+                 <Linkediniconimage src = {Linkedinicon}></Linkediniconimage>
+                </a>
+              </Icongroupwrapper>
+            </ButtongroupWrapper>
+        </BodyWrapper>
       </PageWrapper>
     );
   }
